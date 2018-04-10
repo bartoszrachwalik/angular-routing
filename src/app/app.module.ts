f0b3e73efeb04f34d88c35c955c32859c01c6769
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-10 11:26:37 +0200

    adds router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'servers', component: ServersComponent}
 ];
 
 @NgModule({
@@ -28,7 +28,8 @@ const appRoutes: Routes = [
     ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
