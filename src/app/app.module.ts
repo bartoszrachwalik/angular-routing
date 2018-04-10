183eac65d207f310b70190ecb712348fd6fc366d
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-10 11:21:29 +0200

    adds servers service, paths and user component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {AppComponent} from './app.component';
+import {UsersComponent} from './users/users.component';
+import {ServersComponent} from './servers/servers.component';
+import {HomeComponent} from './home/home.component';
+import {UserComponent} from './users/user/user.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {ServerComponent} from './servers/server/server.component';
+import {ServersService} from './servers/servers.service';
+import {Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-
+const appRoutes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'servers', component: ServersComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +30,8 @@ import { ServerComponent } from './servers/server/server.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
