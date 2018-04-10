425b14304de571f00fa45b35655abb67e7140dc4
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-10 13:25:46 +0200

    practise and making id a number trick

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,11 +10,19 @@ import {ServersService} from '../servers.service';
 export class ServerComponent implements OnInit {
   server: { id: number, status: string };
 
-  constructor(private serversService: ServersService) {
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // plus means id is a number
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
   }
 
 }
